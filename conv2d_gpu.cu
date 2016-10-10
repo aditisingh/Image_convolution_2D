b132608c3e8b171f713acb4c8e4e34510b4d8fb2
Aditi Singh <787aditi@gmail.com>
2016-10-10 10:46:12 -0500

    Cuda 1D array based, issue in convolutions

--- conv2d_gpu.cu
+++ conv2d_gpu.cu
@@ -28,60 +28,59 @@ static void HandleError( cudaError_t err, const char *file, int line ) {
 
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
 
-__device__ void padding(pixel** Pixel_val, int x_coord, int y_coord, int img_width, int img_height, pixel Px) //padding the image,depending on pixel coordinates, can be replaced by reflect for better result //currently zero padding
-{
-	if(x_coord<img_width && y_coord<img_height && x_coord>=0 && y_coord>=0)	
-		Px=Pixel_val[y_coord][x_coord];
+__device__ void padding(pixel* Pixel_val, int x_coord, int y_coord, int img_width, int img_height, pixel Px) //padding the image,depending on pixel coordinates, can be replaced by reflect for better result //currently zero padding
+{	Px.r=0; Px.g=0; Px.b=0;
+	if(x_coord< img_width && y_coord <img_height && x_coord>=0 && y_coord>=0)
+		Px=Pixel_val[y_coord*img_width+x_coord];
 }
 
 
-__global__ void vertical_conv(pixel** Pixel_in, pixel** Pixel_out,int img_wd, int img_ht, float** kernel, int k, size_t pitch)
+__global__ void vertical_conv(pixel* Pixel_in, pixel* Pixel_out,int img_wd, int img_ht, float* kernel, int k)
 {
 	float tmp_r, tmp_g, tmp_b;
-	pixel pix_val;
-	pix_val.r=0;pix_val.g=0;pix_val.b=0;
-	int row=blockIdx.y*blockDim.y + threadIdx.y;            
-	int col = blockIdx.x*blockDim.x + threadIdx.x;
+	int pix_idx=blockIdx.x*blockDim.x + threadIdx.x;
+	int row=(int)(pix_idx/img_wd);
+	int col=pix_idx%img_wd;
+				
 	if(row<img_ht && col<img_wd){
 		tmp_r=0, tmp_g=0, tmp_b=0;
 		for(int l=0;l<k;l++)
-		{	
-			
-			padding(Pixel_in, col, row+l-(k-1)/2, img_wd, img_ht, pix_val);
-			tmp_r+=pix_val.r * kernel[l][0];
-			tmp_b+=pix_val.b * kernel[l][0];
-			tmp_g+=pix_val.g * kernel[l][0];
+		{//doing by 1 D arrays	
+			pixel pix_val;
+			padding(Pixel_in, col, (row+l-(k-1)/2), img_wd, img_ht, pix_val);
+			tmp_r+=pix_val.r * kernel[l];
+			tmp_b+=pix_val.b * kernel[l];
+			tmp_g+=pix_val.g * kernel[l];
 		}
 
-		Pixel_out[row][col].r=tmp_r;
-		Pixel_out[row][col].g=tmp_g;
-		Pixel_out[row][col].b=tmp_b;
+		Pixel_out[pix_idx].r=tmp_r;
+		Pixel_out[pix_idx].g=tmp_g;
+		Pixel_out[pix_idx].b=tmp_b;
 	}
-}
+}			
 
-__global__ void horizontal_conv(pixel** Pixel_in, pixel** Pixel_out, int img_wd, int img_ht, float** kernel, int k)
+
+__global__ void horizontal_conv(pixel* Pixel_in, pixel* Pixel_out, int img_wd, int img_ht, float* kernel, int k)
 {
 	float tmp_r, tmp_b, tmp_g;
-	pixel pix_val;
-	pix_val.r=0;pix_val.g=0;pix_val.b=0;
-
 	//horizontal convolution
-	int row=blockIdx.y*blockDim.y + threadIdx.y;
-
-	int col = blockIdx.x*blockDim.x + threadIdx.x;
+	int pix_idx=blockIdx.x*blockDim.x + threadIdx.x;
+	int row=(int)(pix_idx/img_wd);
+	int col=pix_idx%img_wd;
 	tmp_r=0, tmp_g=0, tmp_b=0;
 	if(row<img_ht && col<img_wd)
 	{
 		for(int l=0; l<k;l++)
 		{
-			padding(Pixel_in, col+l-(k-1)/2, row, img_wd, img_ht, pix_val);
-			tmp_r+=pix_val.r * kernel[0][l];
-			tmp_g+=pix_val.g * kernel[0][l];
-			tmp_b+=pix_val.b * kernel[0][l];
+			pixel pix_val;
+			padding(Pixel_in, col+ l-(k-1)/2, row, img_wd, img_ht, pix_val);
+			tmp_r+=pix_val.r * kernel[l];
+			tmp_g+=pix_val.g * kernel[l];
+			tmp_b+=pix_val.b * kernel[l];
 		}
-		Pixel_out[row][col].r=tmp_r;
-		Pixel_out[row][col].g=tmp_g;
-		Pixel_out[row][col].b=tmp_b;
+		Pixel_out[pix_idx].r=tmp_r;
+		Pixel_out[pix_idx].g=tmp_g;
+		Pixel_out[pix_idx].b=tmp_b;
 	}
 }
 
@@ -123,32 +122,29 @@ int main(int argc, char* argv[])
 
 	if(k%2==0) k++; //to make the size odd
 
-	float **kernel0 = (float **)malloc(k * sizeof(float*)); //y based gaussian
-	float **kernel1 = (float **)malloc(1* sizeof(float*));	//x based gaussian
+	float *kernel0 = (float *)malloc(k * sizeof(float)); //y based gaussian
+	float *kernel1 = (float *)malloc(k * sizeof(float));	//x based gaussian
 
-	for(int i=0;i<k;i++)
-		kernel0[i]=(float*)malloc(1*sizeof(float));
 	
-	kernel1[0]=(float*)malloc(k*sizeof(float));
-
 	float constant1=sqrt(2*M_PI*sigma*sigma);//constants needed to define the kernel
 	float constant2=2*sigma*sigma;
 
 	int mid=floor(k/2);
-	kernel0[mid][0]=1/constant1;
-	kernel1[0][mid]=1/constant1;
+	kernel0[mid]=1/constant1;
+	kernel1[mid]=1/constant1;
 
 	for(int i=0;i<floor(k/2);i++)	//using symmetry from center, to generate the separable kernels 
 	{
-		kernel0[i][0]=((exp(-(floor(k/2)-i)*(floor(k/2)-i)/constant2)))/constant1;
+		kernel0[i]=((exp(-(floor(k/2)-i)*(floor(k/2)-i)/constant2)))/constant1;
 
-		kernel1[0][i]=kernel0[i][0];
+		kernel1[i]=kernel0[i];
 
-		kernel0[k-1-i][0]=kernel0[i][0];
+		kernel0[k-1-i]=kernel0[i];
 
-		kernel1[0][k-1-i]=kernel1[0][i];
+		kernel1[k-1-i]=kernel1[i];
 
 	}
+
 	time_t kernel_generation=time(NULL); //find time taken for kernel generation
 	cout<<" Kernel generation time:"<<double(kernel_generation - start_of_code)<<" sec"<<endl;
 
@@ -186,15 +182,16 @@ int main(int argc, char* argv[])
 	iss2>>word;// this will be image height
 	img_ht=word;
 
-	//storing the pixels as 2d images
-	pixel **Pixel = (pixel**)malloc((img_ht)*sizeof(pixel*));
-	pixel **Pixel_tmp = (pixel **)malloc((img_ht) * sizeof(pixel*)); 
+	pixel *Pixel_out=(pixel*)malloc(img_ht*img_wd*sizeof(pixel));
+	//storing the pixels as lexicographically
+	pixel *Pixel = (pixel*)malloc((img_ht)*(img_wd)*sizeof(pixel));
+	//pixel **Pixel_tmp = (pixel **)malloc((img_ht) * sizeof(pixel*)); 
 	
-	for(int i=0;i<(img_ht);i++)
+	/*for(int i=0;i<(img_ht);i++)
 	{
 		Pixel_tmp[i]=(pixel*)malloc(img_wd*sizeof(pixel));
 		Pixel[i]=(pixel*)malloc((img_wd)*sizeof(pixel));
-	}
+	}*/
 
 
 
@@ -216,20 +213,20 @@ int main(int argc, char* argv[])
 			if(pix_cnt<img_ht*img_wd)
 			{	
 				val =((int)line[i]);
-				row=floor(pix_cnt/img_wd);
-				col=pix_cnt%img_wd;
+				//row=floor(pix_cnt/img_wd);
+				//col=pix_cnt%img_wd;
 				
 				if(cnt%3==0)
 				{		
-					Pixel[row][col].r=val;
+					Pixel[pix_cnt].r=val;
 				}
 				else if(cnt%3==1)
 				{
-					Pixel[row][col].g=val;
+					Pixel[pix_cnt].g=val;
 				}
 				else
 				{
-					Pixel[row][col].b=val;
+					Pixel[pix_cnt].b=val;
 					pix_cnt++;
 				}
 				cnt++;
@@ -243,41 +240,41 @@ int main(int argc, char* argv[])
 
 
 	cudaDeviceProp prop;
-    HANDLE_ERROR(cudaGetDeviceProperties(&prop, 0));
+   	HANDLE_ERROR(cudaGetDeviceProperties(&prop, 0));
 
 
-    int thread_block=sqrt(prop.maxThreadsPerBlock);
-	dim3 DimGrid(ceil(img_wd/thread_block),ceil(img_ht/thread_block),1);
-	dim3 DimBlock(sqrt(prop.maxThreadsPerBlock),sqrt(prop.maxThreadsPerBlock),1);
+    	int thread_block=prop.maxThreadsPerBlock;
+	dim3 DimGrid(ceil(img_ht*img_wd/thread_block),1,1);
+	dim3 DimBlock(thread_block,1,1);
 
 	//allocating gpu memory
 
 
-	pixel **Pixel_tmp_gpu;
+	pixel *Pixel_tmp_gpu;
 
-	pixel **Pixel_gpu;
+	pixel *Pixel_gpu, *Pixel_gpu_res;
    
-
-	HANDLE_ERROR(cudaMalloc((void**)&Pixel_tmp_gpu,img_wd*img_ht*sizeof(pixel))); //allocate space for conv output
-	HANDLE_ERROR(cudaMalloc((void**)&Pixel_gpu,img_wd*img_ht*sizeof(pixel))); //allocate space for image on device
-
-	float **kernel0_gpu, **kernel1_gpu;
+	HANDLE_ERROR(cudaMalloc(&Pixel_gpu_res,img_wd*img_ht*sizeof(pixel))); //allocate space for image on device
+	HANDLE_ERROR(cudaMalloc(&Pixel_tmp_gpu,img_wd*img_ht*sizeof(pixel))); //allocate space for conv output
+	HANDLE_ERROR(cudaMalloc(&Pixel_gpu,img_wd*img_ht*sizeof(pixel))); //allocate space for image on device
 	
-	size_t pitch_k0, pitch_k1;
+	float *kernel0_gpu, *kernel1_gpu;
+	
+	//size_t pitch_k0, pitch_k1;
 
-	HANDLE_ERROR(cudaMallocPitch(&kernel0_gpu, &pitch_k0 ,1*sizeof(float),k));//allocate 1 x k size
+	HANDLE_ERROR(cudaMalloc(&kernel0_gpu, k*sizeof(float)));//allocate 
 
-	HANDLE_ERROR(cudaMallocPitch(&kernel1_gpu, &pitch_k1, k*sizeof(float),1));//allocate k x 1 size
+	HANDLE_ERROR(cudaMalloc(&kernel1_gpu, k*sizeof(float)));//allocate 
 	
 	cout<<"memory allocated"<<endl;
 
 	//copying needed data
 
-	HANDLE_ERROR(cudaMemcpy(Pixel_gpu,Pixel,img_wd*img_ht*sizeof(pixel),cudaMemcpyHostToDevice));
+	HANDLE_ERROR(cudaMemcpy(Pixel_gpu, Pixel, img_wd*img_ht*sizeof(pixel),cudaMemcpyHostToDevice));
 
-	HANDLE_ERROR(cudaMemcpy2D(kernel0_gpu, pitch_k0, kernel0,1*sizeof(float),1*sizeof(float),k,cudaMemcpyHostToDevice));
+	HANDLE_ERROR(cudaMemcpy(kernel0_gpu, kernel0,k*sizeof(float),cudaMemcpyHostToDevice));
 
-	HANDLE_ERROR(cudaMemcpy2D(kernel1_gpu, pitch_k1, kernel1,k*sizeof(float),k*sizeof(float),1,cudaMemcpyHostToDevice));
+	HANDLE_ERROR(cudaMemcpy(kernel1_gpu,kernel1,k*sizeof(float),cudaMemcpyHostToDevice));
 
 	cout<<"memory transfers done"<<endl;
 
@@ -287,34 +284,29 @@ int main(int argc, char* argv[])
 	cout<<" vertical_convolution time: "<<double(vertical_convolution - reading_file)<<"sec"<<endl;
 
 	
-	horizontal_conv<<<DimGrid,DimBlock>>>(Pixel_tmp_gpu, Pixel_gpu, img_wd, img_ht, kernel1_gpu, k);
+	horizontal_conv<<<DimGrid,DimBlock>>>(Pixel_tmp_gpu, Pixel_gpu_res, img_wd, img_ht, kernel1_gpu, k);
 	time_t horizontal_convolution=time(NULL);
 
 	cout<<" horizontal convolution time:" <<double(horizontal_convolution-vertical_convolution)<<" sec"<<endl;
 
-	//size_t pitch;
 
-
-	//HANDLE_ERROR(cudaMemcpy2D(Pixel,img_wd*sizeof(pixel*),Pixel_gpu,pitch,img_wd*sizeof(pixel*),img_ht,cudaMemcpyDeviceToHost));
-	HANDLE_ERROR(cudaMemcpy(Pixel_out,Pixel_gpu,img_wd*img_ht*sizeof(pixel),cudaMemcpyDeviceToHost));
+	HANDLE_ERROR(cudaMemcpy(Pixel_out,Pixel_gpu_res,img_wd*img_ht*sizeof(pixel),cudaMemcpyDeviceToHost));
 
 	//writing this to PPM file
 	ofstream ofs;
 	ofs.open("output_gpu.ppm", ofstream::out);
 	ofs<<"P6\n"<<img_wd<<" "<<img_ht<<"\n"<<max_val<<"\n";
 	
-	for(int j=0; j <img_ht;j++)
+	for(int i=0; i <img_ht*img_wd;i++)
 	{
-		for (int i=0; i<img_wd;i++)
-		{
-			ofs<<Pixel[j][i].r<<Pixel[j][i].g<<Pixel[j][i].b;	//write as ascii
-		}
+		ofs<<Pixel_out[i].r<<Pixel_out[i].g<<Pixel_out[i].b;	//write as ascii
+		//cout<<"did we write?";
 	}
 	
 	
 	ofs.close();
 	time_t end=time(NULL);
-	//cout<<" Saving the result:"<<double(end-horizontal_convolution)<<" sec"<<endl;
+	cout<<" Saving the result:"<<double(end-horizontal_convolution)<<" sec"<<endl;
 
 	//display time taken for different processes
 	cout<<" Total execution time: "<<double(end-start_of_code)<<" sec"<<endl;
